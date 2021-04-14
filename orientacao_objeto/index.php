a77e75f68ab6d9df54254ff84b021ada4c5d4d5e
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2021-04-14 15:58:30 -0300

    Adicionando novos arquivos OO

--- orientacao_objeto/index.php
+++ orientacao_objeto/index.php
@@ -8,18 +8,24 @@ class Post
     public array $comments = [];
     public string $author;
 
+    //Construtor
+    public function __construct($qtdLikes)
+    {
+        $this->likes = $qtdLikes;
+    }
+
     //Declaração de métodos
     public function aumentarLike()
     {
         $this->likes++;
     }
 }
-$post1 = new Post(); //Declaração de um objeto
-$post1->likes = 3; //chamando o atributo/propriedade criado
+$post1 = new Post(5); //Declaração de um objeto
+//$post1->likes = 3; //chamando o atributo/propriedade criado
 $post1->aumentarLike(); //Chamando o método criado
 
-$post2 = new Post();
-$post2->likes = 10;
+$post2 = new Post($qtdLikes = 0); //Pode-se colocar o valor do parâmetro zerado/padrão
+//$post2->likes = 10;
 
 echo "POST1: " . $post1->likes . "<br/>"; //Acessando a propriedade
 echo "POST2: " . $post2->likes . "<br/>"; //Acessando a propriedade
