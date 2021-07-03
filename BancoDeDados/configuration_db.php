88eeced0d310a9bc6e29a403ac366dd19187e668
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2021-07-02 22:56:19 -0300

    Modificacao nos arquivos de configuracao do banco

--- BancoDeDados/configuration_db.php
+++ BancoDeDados/configuration_db.php
@@ -1,18 +1,30 @@
 <?php
+
+//endereco
+//nome_bd
+//usuario
+//senha
+
+
 //Aqui colocamos o servidor em que está o nosso banco de dados,
-//no nosso exemplo é a conexão com um servidor local, portanto localhost
-$servidor = "localhost";
+//no nosso exemplo é a conexão com o pgadmin (poderia ser com o localhost também)
+$servidor = "127.0.0.1";
+
+//Aqui é o nome do banco de dados que foi criado no pgadmin
+$banco = 'Banco_PHP';
+
 //Aqui é o nome de usuário do seu banco de dados, root é o servidor inicial e
 //básico de todo servidor, mas recomenda-se não usar o usuario root e sim criar um novo usuário
 $usuario = "postgres";
+
 //Aqui colocamos a senha do usuário, por padrão o usuário root vem sem senha,
 //mas é altamente recomendável criar uma senha para o usuário root, visto que ele é
 //o que tem mais privilégios no servidor
 $senha ="V@ldir";
 
-//Aqui criamos a conexão utilizando o servidor, usuario e senha,
+//Aqui criamos a conexão utilizando o servidor, nome do banco, usuario e senha,
 //caso dê erro, retorna um erro ao usuário.
-$conexao = pg_connect($servidor, $usuario, $senha) or
+$conexao = pg_connect($servidor, $banco, $usuario, $senha) or
 die ("Não foi possível conectar ao servidor PostGreSQL");
 //caso a conexão seja efetuada com sucesso, exibe uma mensagem ao usuário
 echo "Conexão efetuada com sucesso!!";
--- BancoDeDados/index.php
+++ BancoDeDados/index.php
@@ -1,9 +1,11 @@
 <?php
 
+require_once 'configuration_db.php'; //chamando as configurações criadas do banco de dados
+
 /** No php, para se ter acesso ao banco, temos que criá-lo, ou seja,
  * instanciar um objeto de nome PDO com os dados abaixo...
  */
-$pdo = new PDO("pgsql: dbname = Banco_PHP;host = 127.0.0.1", "postgres", "V@ldir"); // Qual o tipo do banco, o nome do banco, onde ta o banco e a senha dele
+$pdo = new PDO("pgsql: dbname = $banco;host = $servidor", $usuario, $senha); // Qual o tipo do banco, o nome do banco, onde ta o banco e a senha dele
 $sql = $pdo->query('SELECT * FROM usuarios');
 
 $dados = $sql->fetchAll();
