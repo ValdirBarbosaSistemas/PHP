82acd810985ab2ef69033a8513839143c58fbbed
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2021-10-07 12:22:39 -0300

    Modificando o arquivo config.PHP

--- trabalhando_com_arquivos/config.php
+++ trabalhando_com_arquivos/config.php
@@ -1,4 +1,6 @@
 <?php
+/*Por boa prática, o arquivo 'config' serve justamente pra isso... para colocar as configs. do projeto
+e quando quiser usá-las, é só chamar pelo 'require'*/
 $usuario = "Valdir";
 $senha = '123';
 
